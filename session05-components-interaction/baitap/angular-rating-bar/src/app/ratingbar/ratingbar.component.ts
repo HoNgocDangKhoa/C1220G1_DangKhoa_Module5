04244e16e9838731db72f4e9ddacebfa73039b0e
unknown <hongocdangkhoa555@gmail.com>
2021-06-08 08:40:48 +0700

    update

---
+++ session05-components-interaction/baitap/angular-rating-bar/src/app/ratingbar/ratingbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ratingbar',
+  templateUrl: './ratingbar.component.html',
+  styleUrls: ['./ratingbar.component.css']
+})
+export class RatingbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
